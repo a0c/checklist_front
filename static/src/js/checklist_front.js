42977223f38e14523fdacf5a29fb91a7865582f2
Anton Chepurov <anton.chepurov@gmail.com>
2018-06-19 02:53:05 +0300

    increase picture quality: 800x600 => 1152x864

--- static/src/js/checklist_front.js
+++ static/src/js/checklist_front.js
@@ -164,8 +164,8 @@ $(function(){
                 var ctx = canvas.getContext("2d");
                 ctx.drawImage(img, 0, 0);
 
-                var MAX_WIDTH = 800;
-                var MAX_HEIGHT = 600;
+                var MAX_WIDTH = 1152;
+                var MAX_HEIGHT = 864;
 
                 var width = img.width;
                 var height = img.height;
